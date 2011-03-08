2fac096fed1b05d8c64f6abc73dc25a20f0c001a
Jeff Rafter <jeff@socialrange.org>
2011-03-08 13:32:18 -0800

    Adding in couch and couchapp

--- egra/recipes/default.rb
+++ egra/recipes/default.rb
@@ -6,28 +6,21 @@ require_recipe "nodejs"
 include_recipe "nodejs"
 include_recipe "nodejs::npm"
 
-log "===================================================="
-log "Box contains: apt, build-essential, nginx, node, npm"
-log "===================================================="
+require_recipe "python"
+require_recipe "couchdb::source"
 
-template "#{node[:nginx][:dir]}/sites-available/#{'egra'}" do
-  source "nginx.conf.erb"
-end
-
-nginx_site "default" do
-  enable false
-end
-
-nginx_site "egra" do
-  enable true
-end
+python_pip "couchapp"
 
 npm_package "coffee-script"
+
+log "================================================================"
+log "Box contains: apt, build-essential, node, npm, couchdb, couchapp"
+log "================================================================"
 
 # This script is kinda brutal as it runs every second. inotify is too laggy though
 execute "coffee compiling watch script" do
   cwd "/srv/egra"
-  command "cd /srv/egra; nohup scripts/coffee_watcher.sh 1> /srv/egra/log/watch.out.log 2> /srv/egra/log/watch.err.log &"
+  command "cd /srv/egra; nohup scripts/coffee_watcher.sh 1> /srv/egra/log/coffee.out.log 2> /srv/egra/log/coffee.err.log &"
   action :run
 end
 
